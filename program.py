8a2e68a84870417f18b3a433ae284f4db5e31822
elen0 <158768369+elen0@users.noreply.github.com>
2024-02-04 20:06:05 +0500

    Update program.py
    ...

--- program.py
+++ program.py
@@ -2,6 +2,8 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('Yes!')
+    print('Again!')
+    print('I understand nothing')
 
 
 if __name__ == '__main__':
